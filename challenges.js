03da53a139bd22efcf800ac2e9ad001c971ea816
Lucas Dias Coelho <ludico1959@gmail.com>
2022-03-02 17:26:21 -0300

    feat(challenges): complete advanced challenge 19

--- challenges.js
+++ challenges.js
@@ -32,8 +32,8 @@ for (let i = 0; i < vegetais.length; i++)
 ////////////////////////////////////////////////////////
 // 05 (FÁCIL)
 console.log("\n05)____________________________");
-function person(firtsName, lastName, email, tags) {
-  return { firtsName, lastName, email, tags };
+function person(firstName, lastName, email, tags) {
+  return { firstName, lastName, email, tags };
 }
 
 const person01 = person("Fulano", "de Tal", "fulano@email.com", [
@@ -174,7 +174,7 @@ let repositoryArrayFiltered05 = repositoryArray.map((object) => {
 repositoryArrayFiltered05 = repositoryArrayFiltered05.filter(
   (element) => element !== undefined
 );
-console.log(repositoryArrayFiltered05);
+// console.log(repositoryArrayFiltered05);
 
 ////////////////////////////////////////////////////////
 // 18 (AVANÇADO)
@@ -186,4 +186,74 @@ let repositoryArrayFiltered06 = repositoryArray.map((object) => {
 repositoryArrayFiltered06 = repositoryArrayFiltered06.filter(
   (element) => element !== undefined
 );
-console.log(repositoryArrayFiltered06);
+// console.log(repositoryArrayFiltered06);
+
+////////////////////////////////////////////////////////
+// 19 (AVANÇADO)
+console.log("\n19)____________________________");
+const database = [];
+
+const crud = {
+  create: function (person) {
+    database.push(person);
+    return person;
+  },
+
+  read: function (properties) {
+    let filteredDatabase = [];
+
+    if (properties) {
+      filteredDatabase = database.filter((object) => {
+        if (properties.firstName === object.firstName) return object;
+        else if (properties.lastName === object.lastName) return object;
+        else if (properties.email === object.email) return object;
+        else if (properties.tags === object.tags) return object;
+      });
+
+      return filteredDatabase;
+    } else {
+      return database;
+    }
+  },
+
+  update: function (index, person) {
+    const updatedPerson = database[index];
+    updatedPerson.firstName = person.firstName
+      ? person.firstName
+      : updatedPerson.firstName;
+
+    updatedPerson.lastName = person.lastName
+      ? person.lastName
+      : updatedPerson.lastName;
+
+    updatedPerson.email = person.email ? person.email : updatedPerson.email;
+
+    updatedPerson.tags = person.tags ? person.tags : updatedPerson.tags;
+
+    database[index] = updatedPerson;
+  },
+
+  delete: function (index) {
+    delete database[index];
+  },
+};
+
+console.log(
+  crud.create({
+    firstName: "Fulano",
+    lastName: "de Tal",
+    email: "fulano@email.com",
+    tags: ["JS", "Python", "Java"],
+  })
+);
+
+// Testando as funções do CRUD:
+console.log(
+  crud.create({
+    firstName: "Ciclano",
+    lastName: "de Tal",
+    email: "ciclano@email.com",
+    tags: ["Rust", "Python", "C"],
+  })
+);
+console.log(crud.read({ firstName: "Ciclano" }));
